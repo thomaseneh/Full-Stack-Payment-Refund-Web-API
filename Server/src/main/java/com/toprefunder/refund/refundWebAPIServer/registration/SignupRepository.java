2e1b7bda6f21c41229f18eb43080cab41c30898d
Thomas Eneh <thomaseneh@yahoo.com>
2023-12-25 06:04:02 -0600

    ultrack all

--- Server/src/main/java/com/toprefunder/refund/refundWebAPIServer/registration/SignupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.toprefunder.refund.refundWebAPIServer.registration;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SignupRepository extends JpaRepository<SignupEntity, Integer> {
-}
